6693a524ee9a0d28662766f7f6a471e3f455bb0e
Simon Haines <simon@rallysafe.com.au>
2017-03-23 10:41:07 +1100

    Migration of schema view to spectre-based style.

--- view.js
+++ view.js
@@ -9,18 +9,49 @@ class ScalarEditor {
 	this.name = model.name;
 	this.optional = model.optional;
   }
-  render(el) {
+  render(el, schema) {
+	this.el = el;
+	this.el.innerHTML =
+	  `<h3>Any number, e.g. -12.34 or 567</h3>
+	  <form class="form-horizontal">
+	    <div class="form-group">
+	      <div class="col-3">
+	        <label class="form-label" for="input-name">Name</label>
+	      </div>
+	      <div class="col-9">
+	        <input id="input-name" class="form-input" type="text"
+	          placeholder="Name" value="${this.name}"/>
+	      </div>
+	    </div>
+ 	    <div class="form-group">
+	      <div class="col-3"></div>
+	      <div class="col-9">
+	        <label class="form-checkbox">
+	          <input id="input-optional" type="checkbox"
+ 	${this.optional ? "checked" : null}/>
+	          <i class="form-icon"></i>May be absent (becomes 0)
+	        </label>
+	      </div>
+	  </div>
+	  <div class="form-group">
+	    <div class="col-3"></div>
+ 	    <div class="col-9">
+	      <button class="btn" data-action="remove">Remove this</button>
+      </div>
+	  </div>
+	  </form>`;
+
 	let ctx = this;
-	el.text('Any number, e.g. -12.34 or 567.')
-	let container = el.append('div');
-	container
-	  .append('input')
-	  .attr('type', 'checkbox')
-	  .attr('checked', this.optional ? 'checked' : null)
-	  .on('change', function() {
-		ctx.optional = this.checked;
+	d3.select(el).select("#input-name").on('change', function() {
+	  ctx.name = this.value; });
+	d3.select(el).select("#input-optional").on('change', function() {
+	  ctx.optional = this.checked;
+	});
+	d3.select(el).select("[data-action=remove]")
+	  .on('click', function() {
+		d3.event.preventDefault();
+		schema.remove(ctx);
 	  });
-	container.append('span').text('May be absent (becomes 0)');
   }
   commit() {
 	this.model.name = this.name;
@@ -50,126 +81,111 @@ class SchemaView {
   }
 
   render(el) {
-	// el contains the panel div
-	this.el = document.createElement('div');
-	this.el.className = 'panel';
-	el.appendChild(this.el);
+	// TODO move this to a 'workspace' view
+	var root = document.createElement('div');
+	el.appendChild(root);
 
+	this.el = root;
+	this.el.className = 'card shadow-0';
 	this.renderViewer();
   }
 
   renderViewer() {
+	this.el.innerHTML =
+	  `<div class="card-header">
+	    <div class="card-tools float-right">
+ 	      <a href="#" class="btn btn-link" title="edit">Edit</a>
+	      <a href="#" class="btn btn-primary" title="close">Close</a>
+	    </div>
+	    <div class="card-title">${this.name}</div>
+	  </div>
+	  <div class="card-body"></div>
+	  <div class="card-footer"></div>`;
+	
 	let ctx = this;
 	let el = d3.select(this.el);
-    el.selectAll('*').remove();
-	el.append('div')
-	  .attr('class', 'toolbar')
-	  .selectAll('button')
-	  .data([
-	    { text: 'Edit', action: this.renderEditor.bind(this) },
-	  ])
-	  .enter()
-	  .append('button').attr('type', 'button').attr('class', 'small')
-	  .text((d) => d.text)
-  	  .on('click', (d) => d.action());
-	el.append('h1').text(this.name);
+
+	el.select('[title=edit]').on('click', this.renderEditor.bind(this));
+	// TODO 'close'
 
 	// TODO create a drag/drop element
-	el.append('div').attr('min-height', '50px').text('drop zone');
+	el.select('.card-body').text('DROP ZONE');
 
-	el.append('div').attr('class', 'tags')
-	  .selectAll('.tag')
+	el.select('.card-footer')
+	  .selectAll('.chip')
 	  .data(this.schema.tags)
 	  .enter()
-	  .append('span').attr('class', 'tag').text((d) => d);
+	  .append('label').attr('class', 'chip').text((d) => d);
   }
 
   renderEditor() {
+	this.el.innerHTML =
+  	  `<div class="card-header">
+	    <div class="card-tools float-right">
+ 	      <a href="#" class="btn btn-link" title="save">Save</a>
+	      <a href="#" class="btn btn-primary" title="close">Close</a>
+	    </div>
+	    <form class="col-5 form-horizontal">
+	    <input class="form-input" type="text" title="name"
+   	      placeholder="Name" value="${this.name}"/>
+	  </div>
+	  <div class="card-body"></div>
+	  <div class="card-footer"></div>`;
+
+	  
 	let ctx = this;
 	let el = d3.select(this.el);
-	el.selectAll('*').remove();
-	el.append('div')
-	  .attr('class', 'toolbar')
-	  .selectAll('button')
-	  .data([
-	    { text: 'Save', action: this.commit.bind(this) },
-		{ text: 'Cancel', action: this.rollback.bind(this) }
-	  ])
-	  .enter()
-	  .append('button')
-	  .attr('type', 'button').attr('class', 'small')
-	  .text((d) => d.text)
-	  .on('click', (d) => d.action());
 
-	// Title editor
-	el.append('input')
-	  .attr('type', 'text')
-	  .attr('class', 'name-editor')
-	  .attr('value', this.name)
-	  .attr('placeholder', 'Schema name')
-	  .on('change', function() { ctx.name = this.value; });
+	el.select('[title=save]').on('click', this.commit.bind(this));
+	el.select('[title=close]').on('click', this.rollback.bind(this));
+	el.select('[title=name]').on('change', function() {
+	  ctx.name = this.value;
+	});
 
-	var widgets = el.append('div').attr('class', 'widgets');
+	// Widgets
+	var widgets = el.select('.card-body')
+		.append('div').attr('class', 'widgets');
 	this.renderWidgets(widgets);
+	  
 
-	// Control for adding a new attribute
+	  // Control for adding a new attribute
 	let editors = [];
-	viewRegistry.forEach((v) => editors.push(v));
-	let attribute = el.append('div')
-		.attr('class', 'attribute');
-	attribute
-	  .append('span')
-	  .attr('class', 'attribute-name')
-	  .text('Add a new attribute:');
-	let attributeValue = attribute
-	  .append('span')
-		.attr('class', 'attribute-value attribute-ctrl');
-	attributeValue
-	  .append('select')
-	  .selectAll('option')
-	  .data(editors)
+	  viewRegistry.forEach((v) => editors.push(v));
+	  el.select('.card-body').append('form')
+		  .attr('class', 'form-horizontal col-5')
+		  .html(`<div class="form-group">
+		  <div class="col-3">
+		  <label class="form-label" for="attribute-add">Add attribute</label>
+		  </div>
+		  <div class="col-9">
+		  <select id="attribute-add" class="form-select">
+		  </select>
+		  </div>
+				</div>`)
+		  .select('#attribute-add')
+		  .on('change', function() { alert('add!'); })
+	      .selectAll('option')
+		  .data(editors)
 	  .enter()
 	  .append('option')
 	  .attr('value', (d,i) => i)
-      .text((d) => d.prototype.description);
-	attributeValue
-	  .append('button').attr('type', 'button').text('add')
-	  .on('click', function(d, i) { alert('add'); });
+		  .text((d) => d.prototype.description);
   }
 
   renderWidgets(widgets) {
+	let ctx = this;
 	widgets
 	  .selectAll('div.widget')
 	  .data(this.widgets)
 	  .enter()
 	  .append((d, i) => {
-		var widget = d3.select(document.createElement('div'))
-		    .attr('class', 'attribute');
-		let widgetName = widget
-		  .append('span')
-			.attr('class', 'attribute-name');
-		widgetName
- 		    .append('input')
-		    .attr('type', 'text')
-		    .attr('placeholder', 'Name')
-		  .attr('value', d.name)
-		  .on('change', function() {
-			d.name = this.value
-		  });
-		widgetName
-		  .append('button').attr('class', 'small')
-		  .text('Remove')
-		  .on('click', function(d) {
-			alert('remove!');
-		  });
-
-		var editor = widget
-			.append('span')
-		    .attr('class', 'attribute-value');
-		d.render(editor);
-		
-		return widget.node(0);
+		let el = document.createElement('div');
+		el.className = 'widget col-5';
+		d.render(el, ctx);
+		return el;
 	  });
+
+	widgets.append('div').attr('class', 'divider');
   }
 
   commit() {
@@ -182,6 +198,10 @@ class SchemaView {
 	this.name = this.schema.name;
 	this.widgets.forEach((w) => w.rollback());
 	this.renderViewer();
+  }
+
+  remove(item) {
+	alert('Remove: ' + item.name);
   }
 
   static register(domain, editor) {
